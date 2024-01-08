26c8f5af1477c2a5bfffd86ccdfe515eab1a9e54
Sonjulking <bomedame97@gmail.com>
2024-01-08 17:36:37 +0900

    hello zito

--- src/main/java/com/ggck/vware/MainController.java
+++ src/main/java/com/ggck/vware/MainController.java
@@ -2,14 +2,13 @@ package com.ggck.vware;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
   @GetMapping("/")
   public String root() {
-    return "vbti/main";
+    return "EGG/main";
   }
 
 }
