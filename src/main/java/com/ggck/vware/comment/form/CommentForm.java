1b0ab0212e1d69ca80b1920bb65a0cfda5fbaa8c
Sonjulking <bomedame97@gmail.com>
2024-03-05 05:56:44 +0900

    세부 css

--- src/main/java/com/ggck/vware/comment/form/CommentForm.java
+++ src/main/java/com/ggck/vware/comment/form/CommentForm.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 public class CommentForm {
 
   @NotEmpty(message = "내용은 필수항목입니다.")
-  @Size(max = 200)
+  @Size(max = 200, message = "200자이하로 작성해주세요.")
   private String content;
 
 }
